dbee89b631f4407aea7917b9671faf08d3fa1910
Paull Stanley <paull.stanley@me.com>
2022-09-19 01:02:54 -0400

    Refactoring

---
+++ CoreDataPlugin/Tests/CoreDataPluginTests/CoreDataPluginTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CoreDataPlugin
+
+final class CoreDataPluginTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CoreDataPlugin().text, "Hello, World!")
+    }
+}
